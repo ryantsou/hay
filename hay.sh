5c476982bb428378639332eaa01fb7d6b747e930
ryantsou <62847028+ryantsou@users.noreply.github.com>
2024-03-18 10:14:36 +0300

    Create hay.sh

---
+++ hay.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+wish_birthday() {
+    clear
+    echo -e "\n\n\n\n"
+     echo "                  "
+    echo "                   ❤️    ❤️ "
+    echo "                 ❤️   ❤️   ❤️"
+    echo "🎉🎂 Joyeux Anniversaire Michelle manja kely! 🎂🎉 "
+    echo "                   ❤️     ❤️"
+    echo "                    ❤️  ❤️"
+    echo "                      ❤️ "
+    sleep 1
+}
+
+
+wish_birthday
